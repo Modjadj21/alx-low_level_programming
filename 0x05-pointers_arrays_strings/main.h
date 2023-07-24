65e83558192129f8b9b997d8b70be5f9ee274643
Modjadj21 <modjadjigloriat@gmail.com>
2023-07-24 13:27:26 -0700

    Corrected

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototype */
+/* Function prototype */;
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,5 +14,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-char *generate_password(void);
 #endif /* MAIN_H */
